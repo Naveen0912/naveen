a14f84c98bea5be2031a7430a6c3a7354e13ae05
Naveen0912 <naveenkumar912269@gmail.com>
2021-12-15 06:36:15 +0530

    win

---
+++ src/org/str/CharStr.java
@@ -0,0 +1,50 @@
+package org.str;
+
+
+public class CharStr {
+	public static void main(String[] args) {
+		
+		String s= "Hello welcome world";
+		
+		char[] ch = s.toCharArray();
+		
+		int count=0;
+		
+		for (char d : ch) {
+			
+			
+			
+			
+			if (d=='a'|| d=='e'||d=='i'||d=='o'||d=='u') {
+				
+				
+				count++;
+				
+				System.out.println(d+""+""+count);
+				
+				
+			} 
+
+			
+			
+			
+		}
+		
+
+		
+		
+		
+		
+		
+		
+			
+		}
+		
+		
+	
+		
+	}
+	
+	
+
+
---
+++ src/org/str/Str.java
@@ -0,0 +1,25 @@
+package org.str;
+
+public class Str {
+	public static void main(String[] args) {
+		
+		String s= "java";
+		String s1="selenium";
+		
+		String temp=s;
+		String temp1=s1;
+		s1=temp;
+		s=temp1;
+		
+		System.out.println("s="+s);
+		System.out.println("s1="+s1);
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
